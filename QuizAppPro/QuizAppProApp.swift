65e0e5f68d2e367154abb464f347892f3ecc13ac
Sabrina C. Sales <sacsales@icloud.com>
2023-07-23 17:29:55 +0400

    Initial Commit

---
+++ QuizAppPro/QuizAppProApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppProApp.swift
+//  QuizAppPro
+//
+//  Created by Sabrina C. Sales on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
